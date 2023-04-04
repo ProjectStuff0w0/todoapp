9187435ebbc29710fe20f8ca67997a7ce826e0f9
HiddenMachine Laptop <108694711+HiddenMachine3@users.noreply.github.com>
2023-04-04 13:41:27 +0530

    Final project

---
+++ app/todoList.js
@@ -0,0 +1,31 @@
+//'use client';
+import Todo from "./todo";
+
+const getTodos = async () =>
+{
+    let todos = await fetch("http://127.0.0.1:3001/api/todo/list",{cache:'no-cache'});
+    //can't shorten url since it is a server component
+    return todos.json();
+}
+
+export default async function TodoList()
+{
+    const { todos } = await getTodos();
+    console.log(todos);
+    
+    return (
+        <div>
+          <ul style={{ listStyleType: "none", padding: 0 }}>
+            {todos.map((t) => {
+              return (
+                <li key={t.id} style={{ padding: "5px 0" }}>
+                  <Todo todo={t}/>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      );
+
+
+}
